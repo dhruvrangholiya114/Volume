1a753ca2a3504b9c91a229d7d8c56af2502ec039
dhruvrangholiya114 <130462930+dhruvrangholiya114@users.noreply.github.com>
2023-05-19 17:24:51 +0530

    Add files via upload

---
+++ volume_3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+main()
+{
+	enum weekday{Monday=1, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday};
+	
+    printf("Monday = %d", Monday);
+	printf("\nTuesday = %d", Tuesday);
+	printf("\nWednesday = %d", Wednesday);
+	printf("\nThursday = %d", Thursday);
+	printf("\nFriday = %d", Friday);
+	printf("\nSaturday = %d", Saturday);
+	printf("\nSunday = %d", Sunday);
+}
+
